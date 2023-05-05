8876b5489278ff3563745a490c311bfd2204768f
Exe303 <127712981+Exe303@users.noreply.github.com>
2023-05-05 23:50:02 +0700

    Update ssh-vpn.sh

--- FILE/SSH/ssh-vpn.sh
+++ FILE/SSH/ssh-vpn.sh
@@ -228,9 +228,6 @@ sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 
 #OpenVPN
 wget https://raw.githubusercontent.com/Exe303/Crop/main/FILE/OPENVPN/vpn.sh &&  chmod +x vpn.sh && ./vpn.sh
-#Install Server UDP
-wget https://gitlab.com/Exe302/crot/-/raw/main/udp-custom.sh && bash udp-custom.sh
-rm -f /root/udp-custom.sh
 
 # install fail2ban
 apt -y install fail2ban
